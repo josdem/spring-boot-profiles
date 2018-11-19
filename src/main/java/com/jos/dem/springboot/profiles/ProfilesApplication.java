5756f16426456ef4dbaf19d8db575c223a0946ad
josdem <joseluis.delacruz@gmail.com>
2018-11-19 15:29:32 -0500

    Adding missing variable

--- src/main/java/com/jos/dem/springboot/profiles/ProfilesApplication.java
+++ src/main/java/com/jos/dem/springboot/profiles/ProfilesApplication.java
@@ -2,11 +2,15 @@ package com.jos.dem.springboot.profiles;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProfilesApplication {
+
+  @Value("${user.url}")
+  private String userUrl;
 
   public static void main(String[] args) {
     SpringApplication.run(ProfilesApplication.class, args);
