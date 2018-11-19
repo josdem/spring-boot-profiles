6a157b2e34769ab4cbccb181123ad23a8c57e776
josdem <joseluis.delacruz@gmail.com>
2018-11-19 15:27:20 -0500

    Adding command runner

--- src/main/java/com/jos/dem/springboot/profiles/ProfilesApplication.java
+++ src/main/java/com/jos/dem/springboot/profiles/ProfilesApplication.java
@@ -1,5 +1,7 @@
 package com.jos.dem.springboot.profiles;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +10,13 @@ public class ProfilesApplication {
 
   public static void main(String[] args) {
     SpringApplication.run(ProfilesApplication.class, args);
+  }
+
+  @Bean
+  CommandLineRunner run(){
+    return args -> {
+      System.out.println("Environment: " + userUrl);
+    };
   }
 
 }
